9574f8e3ab0fa910bad30ffbc7b3cc1a90bf3b81
alokkumar-dev <alokshakya2601@gmail.com>
2021-02-03 20:53:01 +0530

    asdf

--- add.php
+++ add.php
@@ -3,12 +3,12 @@
     <h2>Add New Record</h2>
     <form class="post-form" action="savedata.php" method="post">
         <div class="form-group">
-            <label>Name</label>
-            <input type="text" name="sname" />
+            <label for="name">Name</label>
+            <input type="text" id="name" name="sname" />
         </div>
         <div class="form-group">
-            <label>Address</label>
-            <input type="text" name="saddress" />
+            <label for="address">Address</label>
+            <input type="text" id="address" name="saddress" />
         </div>
         <div class="form-group" id="class_drop">
             <label>Class</label>
@@ -29,10 +29,10 @@
                </select>
         </div>
         <div class="form-group">
-            <label>Phone</label>
-            <input type="text" name="sphone" />
+            <label for="phone">Phone</label>
+            <input type="text" id="phone" name="sphone" />
         </div>
-        <input class="submit" type="submit" value="Save"  />
+        <input class="submit" type="submit" value="Save"/>
     </form>
 </div>
 </div>
